302cc8f5ac8d00222c69cbf9c52946deb112893a
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-12-21 12:18:53 -0300

    conexão com banco mongo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 let express = require("express");
 let app = express();
+let mongoose = require("mongoose");
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/guiapics").then(() => {
+    console.log("Conectado com sucesso!")
+}).catch((error) => {
+    console.log("Errosss: " + error)
+});
 
 app.get("/", (req, res) => {
     res.json({});
